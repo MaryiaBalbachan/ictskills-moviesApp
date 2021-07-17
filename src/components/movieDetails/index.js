4a9bc2e87d754c4e2f2d1e1997a00bacb89e833c
MaryiaBalbachan <mashabalbachan@gmail.com>
2021-07-17 12:24:29 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -52,6 +52,9 @@ const MovieDetails = ( props) => {
           </li>
         ))}
       </Paper>
+
+     
+
       <Paper component="ul" className={classes.root}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
@@ -63,6 +66,17 @@ const MovieDetails = ( props) => {
           label={`${movie.vote_average} (${movie.vote_count}`}
         />
         <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+
+      <Paper component="ul" className={classes.root}>
+        <li>
+          <Chip label="Production Countries" className={classes.chip} color="primary" />
+        </li>
+        {movie.production_countries.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} className={classes.chip} />
+          </li>
+        ))}
       </Paper>
 
       <Fab
